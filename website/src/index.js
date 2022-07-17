0376c43a3437f2df61f4b3d07d960dce84c722b5
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2022-07-16 17:32:16 -0700

    Added Routes

--- website/src/index.js
+++ website/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function About () {
   return(
@@ -17,18 +17,24 @@ function Contact () {
   )
 }
 
+function Experiences () {
+  return(
+    <p>Experiences Component</p>
+  )
+}
+
 //Website Structure
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="AboutMe" element={<About />} />
-    <Route path="Experiences" element={<Experiences />} />
-    <Route path="Contact" element={<Contact />} />
-  </Routes>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/AboutMe" element={<About />} />
+      <Route path="/Experiences" element={<Experiences />} />
+      <Route path="/Contact" element={<Contact />} />
+    </Routes>
   </BrowserRouter>
 
 );
