5a2d9f4c55df1587d43b187285de988859a107c8
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2022-09-03 08:02:26 -0700

    Update Header.js

--- website/src/Components/Shared/Header.js
+++ website/src/Components/Shared/Header.js
@@ -6,6 +6,110 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { styled, alpha } from '@mui/material/styles';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Divider from '@mui/material/Divider';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === 'light' ? 'rgb(00, 00, 00)' : theme.palette.grey[300],
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
+
+function CustomizedMenus() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="demo-customized-button"
+        aria-controls={open ? 'demo-customized-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        variant="contained"
+        disableElevation
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
+      >
+      <MenuIcon />
+
+      </Button>
+      <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          'aria-labelledby': 'demo-customized-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}  disableRipple>
+          <a href="/">Home</a>
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <a href="/#/AboutMe">About Me</a>
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <a href="/#/WhatIDo">What I Do</a>
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <a href="/#/WhoIAm">Who I Am</a>
+        </MenuItem>
+        <MenuItem onClick={handleClose} disableRipple>
+          <a href="/#/Experiences">Experiences</a>
+        </MenuItem>
+        <Divider sx={{ my: 0.5 }} />
+        <MenuItem onClick={handleClose} disableRipple>
+          <a href="/#/Contact">Contact</a>
+        </MenuItem>
+      </StyledMenu>
+    </div>
+  );
+}
 
 export default function ButtonAppBar() {
   return (
@@ -19,15 +123,9 @@ export default function ButtonAppBar() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+          <CustomizedMenus />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <a href="/">Home</a>
-          <a href="/#/AboutMe">About Me</a>
-          <a href="/#/WhatIDo">What I Do</a>
-          <a href="/#/WhoIAm">Who I Am</a>
-          <a href="/#/Experiences">Experiences</a>
-          <a href="/#/Contact">Contact</a>
           </Typography>
           <Button color="inherit">Welcome!</Button>
         </Toolbar>
